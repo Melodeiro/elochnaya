1142979942420f2fa7d8c095b8d4c7a34a4b58b8
Melodeiro <d1b@bk.ru>
2020-12-22 05:15:54 +0300

    Add utm_cookies.js

---
+++ utm_cookies.js
@@ -0,0 +1,11 @@
+if (true) { // if (typeof URLSearchParams == 'undefined') {
+    var script = document.createElement('script');
+    script.src = "https://unpkg.com/@ungap/url-search-params@0.2.2/min.js";
+    document.head.appendChild(script);
+}
+
+$.cookie("utm_source", urlParams.get("utm_source") || $.cookie("utm_source") || "", { expires: 30 });
+$.cookie("utm_medium", urlParams.get("utm_medium") || $.cookie("utm_medium") || "", { expires: 30 });
+$.cookie("utm_campaign", urlParams.get("utm_campaign") || $.cookie("utm_campaign") || "", { expires: 30 });
+$.cookie("utm_term", urlParams.get("utm_term") || $.cookie("utm_term") || "", { expires: 30 });
+$.cookie("utm_content", urlParams.get("utm_content") || $.cookie("utm_content") || "", { expires: 30 });
