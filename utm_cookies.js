48e4cd352f63686e4323278a466b2424852d7676
Melodeiro <d1b@bk.ru>
2020-12-22 05:18:30 +0300

    Fix missing urlParams

--- utm_cookies.js
+++ utm_cookies.js
@@ -3,7 +3,7 @@ if (true) { // if (typeof URLSearchParams == 'undefined') {
     script.src = "https://unpkg.com/@ungap/url-search-params@0.2.2/min.js";
     document.head.appendChild(script);
 }
-
+const urlParams = new URLSearchParams(window.location.search);
 $.cookie("utm_source", urlParams.get("utm_source") || $.cookie("utm_source") || "", { expires: 30 });
 $.cookie("utm_medium", urlParams.get("utm_medium") || $.cookie("utm_medium") || "", { expires: 30 });
 $.cookie("utm_campaign", urlParams.get("utm_campaign") || $.cookie("utm_campaign") || "", { expires: 30 });
